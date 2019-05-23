3b890fdc3e5d0c36b637ee6aa658496ad07462bf
Jerome Laroche <jerome.laroche@ibis.ulaval.ca>
2019-05-23 11:15:56 -0400

    Remove quality_control directory

--- makeDir_V2.sh
+++ makeDir_V2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "\nMake structure directory: data, barcodes refgenome, results, control_quality\n"
-for i in data reject barcodes refgenome results control_quality
+for i in data reject barcodes refgenome results
 do
 mkdir $i
 done
